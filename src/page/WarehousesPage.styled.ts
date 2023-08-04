f97ef50f94c0ebf5d68fbfc5307e4efcd14fda5b
Andrii Utkin <105128068+AndreyNik2@users.noreply.github.com>
2023-08-04 19:15:06 +0300

    Aded styles

--- src/page/WarehousesPage.styled.ts
+++ src/page/WarehousesPage.styled.ts
@@ -1,10 +1,12 @@
 import styled from "@emotion/styled";
 import { Typography, TextField, Button, Box } from "@mui/material";
 
-export const Main = styled.main`
-  display: flex;
-  align-items: center;
+export const MainContainer = styled(Box)`
+  margin-top: 16px;
   width: 375px;
+  margin-left: auto;
+  margin-right: auto;
+
   @media screen and (min-width: 768px) {
     width: 768px;
   }
@@ -47,39 +49,33 @@ export const Btn = styled(Button)`
   margin-top: 20px;
 `;
 
-export const FlexContainerInfo = styled(Box)`
-  @media screen and (min-width: 768px) {
-    display: flex;
-    gap: 20px;
-    justify-content: center;
-    
-  }
-  @media screen and (min-width: 1440px) {
-  }
-`;
-
-export const ContainerInfo = styled(Box)`
-  width: 313px;
-  min-height: 200px;
+export const LocalityContainer = styled(Box)`
+  width: 343px;
   margin-left: auto;
   margin-right: auto;
   margin-top: 20px;
-  padding: 15px;
-  border-radius: 8px;
-  border: 1px solid #999999;
   @media screen and (min-width: 768px) {
-    display: inline-flex;
-    width: calc((100% - 60px/2));
+    width: 700px;
   }
   @media screen and (min-width: 1440px) {
+    width: 1000px;
   }
 `;
 
-export const BtnNumber = styled(Button)`
-  width: 100%;
-  &:not(last-child) {
-    margin-bottom: 10px;
-  }
+export const BtnCities = styled(Button)`
+  display: block;
+  margin-top: 5px;
+`;
+
+export const BtnWarehouses = styled(Button)`
+  display: block;
+  margin-top: 5px;
+`;
+
+export const NoWarehouseMessage = styled(Typography)`
+margin-top: 20px;
+  text-align: center;
+
 `;
 
 export const Footer = styled.footer`
